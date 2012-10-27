19920e06d8eaff77c32e1aaf31cacc04c2966f2b
welsonla <wyc.jar@gmail.com>
2012-10-27 13:32:04 +0800

    通过token获取用户信息

---
+++ HelloDouban/DouAuth.h
@@ -0,0 +1,13 @@
+//
+//  DouAuth.h
+//  HelloDouban
+//
+//  Created by 万业超 on 12-10-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DouAuth : NSObject
+
+@end
---
+++ HelloDouban/DouAuth.m
@@ -0,0 +1,13 @@
+//
+//  DouAuth.m
+//  HelloDouban
+//
+//  Created by 万业超 on 12-10-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DouAuth.h"
+
+@implementation DouAuth
+
+@end
